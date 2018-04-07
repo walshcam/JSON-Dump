7d3f2c96f2a108c65f7314dc24a67e7ae072e03c
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-07 08:12:58 -0400

    created function that add gifs after button click

--- JSONDump/assets/javascript/script.js
+++ JSONDump/assets/javascript/script.js
@@ -1,29 +1,46 @@
 // ****GLOBAL VARIABLES
 let gifSearch;
+let gifCount = 0;
+let rowCount = 1;
 
 // Form Javascript
 
 //     AJAX Function
 
 function displayTastyGIFs() {
+    //Ensure function is working
     console.log("DisplayTastyGIFs Function")
 
+    //API Key for giphy.com
     let apiKey = "jSNwEu8AaYEGy9B8SpoaztT8M3KsQrrO"
+
+    //create URL
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&apikey=" + apiKey;
 
+    //AJAX method
     $.ajax({
       url: queryURL,
       method: "GET"
+
+      //Promise
     }).then(function(response) {
+      //ensure ajax is working
       console.log(response)
+      //create saved button from response
       renderButtons();
+      showGIF();
     });
 }
 
+// Submit Form Button Clicked
 $("form").submit(function() {
+    //verify button click worked
     console.log("Button Clicked")
+    // save form input as gifSearch
     gifSearch = $("input").val();
+    //verify the saved input is correct and working
     console.log("GIF Search is: " + gifSearch);
+    //transfer searched input to AJAX
     displayTastyGIFs()
 
 });
@@ -31,12 +48,9 @@ $("form").submit(function() {
 //     Responsive Button Function  
 function renderButtons() {
 
-  // Looping through the array of movies
-  for (var i = 0; i < movies.length; i++) {
-
     // Then dynamicaly generating buttons for each movie in the array
     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
+    let a = $("<button>");
     // Adding a class of movie-btn to our button
     a.addClass("savedButton");
     // Adding a data-attribute
@@ -44,15 +58,8 @@ function renderButtons() {
     // Providing the initial button text
     a.text(gifSearch);
     // Adding the button to the buttons-view div
-    $("#buttons-view").append(a);
-  }
+    $("#buttonArea").append(a);
 }
-
-//     Type In Search 
-
-//     Click Form Button 
-
-//     Add Button to Button List 
 
 //     Remove Display of GIFs 
 
@@ -63,6 +70,41 @@ function renderButtons() {
 //     Remove Display of original GIFs 
 
 //     Display GIFs
+function showGIF() {
+
+    // Create new row if 6 pictures are in the first one
+    if (gifCount % 6){
+      rowCount++;
+      let row = $("<div>").addClass("row").attr("value",rowCount);
+      row.insertAfter($("row").attr("value",rowCount-1));
+      let gifDiv = $("<div>");
+      gifDiv.addClass("col-md-2");
+      $(row).append(gifDiv);
+    }
+    //Add one to the gif count to keep track of the number of images
+    gifCount++;
+
+
+    //create div that contains the image
+    let gifDiv = $("<div>");
+    gifDiv.addClass("col-md-2");
+    $(row).append(gifDiv);
+
+    // Creating and storing an image tag
+    let gifImage = $("<img>");
+
+    // List of attributes added to each gif
+    gifImage.attr("src", imageUrl);
+    gifImage.attr("data-still","#");
+    gifImage.attr("data-animate","#");
+    gifImage.attr("data-state","still");
+
+    //class added to each gif
+    gifImage.addClass("gif");
+
+    // Prepending the catImage to the images div
+    $(gifDiv).append(gifImage);
+}
 
 
 // GIFs
